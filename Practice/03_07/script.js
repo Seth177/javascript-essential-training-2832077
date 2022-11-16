0d86b9d826a446223aa4d5a3337cf6fadfda4865
Seth <sethtan845@gmail.com>
2022-11-16 20:40:17 +0800

    creating objects

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,26 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+
+const box = {
+    name:"Cool box",
+    color:"red",
+    volume: 20,
+    sand: {
+        top:30,
+        bottom:20,
+        left:10,
+        right:10
+    },
+
+    lidOpen: false,
+    toggleLid: function (lidStatus) {
+    this.lidOpen = lidStatus;
+    updateBackpack(`The lid has been open.`);
+    },
+
+
+
+
+};
