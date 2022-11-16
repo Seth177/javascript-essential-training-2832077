c3f4e5d9dc1079a62b6f17bc26a45b9d63c03df7
Seth <sethtan845@gmail.com>
2022-11-16 23:13:12 +0800

    create class and multiple objects using that class

--- Practice/03_12/pencil case.js
+++ Practice/03_12/pencil case.js
@@ -34,3 +34,6 @@ class Pencilcase {
     }
 
 }
+
+
+export default Pencilcase;
--- Practice/03_12/script.js
+++ Practice/03_12/script.js
@@ -7,4 +7,49 @@
  * - Test the objecs by calling their properties and using their methods in the console.
  */
 
+import Pencilcase from "./pencil case.js";
+
+const item = new Pencilcase (
+    "Deluxe",
+    30,
+    "blue",
+    40,
+    30,
+    20,
+    10,
+    false
+)
+
+const item2 = new Pencilcase(
+    "Special",
+    50,
+    "red",
+    20,
+    40,
+    10,
+    30,
+    true
+
+
+)
+
+const item3 = new Pencilcase(
+    "Recent",
+    30,
+    "Maroon",
+    10,
+    20,
+    10,
+    30,
+    true
+
+
+)
+
+console.log("Object",item)
+console.log("Another Object",item2)
+console.log("Last Object",item3)
+
+
+console.log(item.color)
 
