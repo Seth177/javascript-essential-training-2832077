5c84156aad88f6ba901a5ec7af5c5bb23a57b927
Seth <sethtan845@gmail.com>
2022-11-16 23:00:38 +0800

    creating class

---
+++ Practice/03_12/pencil case.js
@@ -0,0 +1,36 @@
+class Pencilcase {
+    constructor(
+        name,
+        size,
+        color,
+        height,
+        width,
+        thick,
+        volume,
+        zipOpen,
+    ){
+        this.name = name;
+        this.size = size;
+        this.color = color;
+        this.volume = volume;
+        this.length = {
+            top: height,
+            length: width,
+            thickness: thick,
+
+        }
+        this.zipOpen = zipOpen;
+    
+    }
+
+    togglelid(lidStatus) {
+        this.zipOpen = lidStatus;
+    }
+
+    newmeasurements(height,length){
+        this.length.top = height;
+        this.length.length = width;
+        this.length.thickness = thick;
+    }
+
+}
